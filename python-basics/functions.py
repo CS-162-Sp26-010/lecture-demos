616adb1ca32e464387b2f581c489339bdfed63e3
Alexander Guyer <guyera@oregonstate.edu>
2026-04-03 10:54:20 -0700

    4/3

---
+++ python-basics/functions.py
@@ -0,0 +1,12 @@
+def add(value_a: float, value_b: float) -> float:
+    value_c = value_a + value_b
+    return value_c
+
+def main() -> None:
+    # Function bodies MUST be indented in Python
+    print('Hello, World!')
+
+    print(add(7.1, 3.14))
+
+if __name__ == '__main__':
+    main()
--- python-basics/variables.py
+++ python-basics/variables.py
@@ -41,14 +41,15 @@ def main() -> None:
     # the right and stores it in the variable on
     # the left. If the variable on the left doesn't
     # already exist, it automatically creates it.
-    my_cool_variable = (3.14 + 7.1) * 7.1 - 2 / 2
+    my_cool_variable: float = (3.14 + 7.1) * 7.1 - 2 / 2
 
     print(my_cool_variable)
 
     # In python, a variable TECHNICALLY can change
     # types throughout a program.
 
-    my_cool_variable = 'Hello'
+    # my_cool_variable = 'Hello'
+    my_cool_variable = 1
 
     # HOWEVER, you SHOULDN'T do this, in most cases.
 
@@ -59,6 +60,25 @@ def main() -> None:
     # Although the above line of code (my_cool_variable = 'Hello')
     # is okay from the interpreter's perspective, Mypy
     # will complain about it, prompting us to fix it.
+
+    # static: before runtime
+    # dynamic: at runtime
+
+    print(my_cool_variable)
+
+    name = 'John'
+    favorite_food = 'pizza'
+    
+    print(f'{1 * 3 / 2.1}\'s favorite food is {favorite_food}')
+
+    # Type casting is when you transform an expression into a new
+    # expression of a different type. It's a "conversion"
+
+    x = 2
+    y = 5
+    print(float(x) / y)
+
+    print(int(3.999)) # Prints 3. Rule is truncation.
 
 if __name__ == '__main__':
     main()
