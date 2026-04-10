48fd051227696852c2f1e9f2bbe8259eae4e1d4d
Alexander Guyer <guyera@oregonstate.edu>
2026-04-10 10:54:31 -0700

    4/10

---
+++ python-basics/lists.py
@@ -0,0 +1,65 @@
+# import math
+from math import sqrt, sin
+
+def modify_list(the_list: list[int]) -> None:
+    the_list[0] = 1000
+
+def main() -> None:
+    print(sqrt(100))
+
+    # A list is a sequence of objects that supports random access (direct
+    # access). It also supports adding / removing objects.
+    # [1, 7, 2, -1, 4]
+
+    # Create list
+    my_list = [1, 7, 2, -1, 4]
+
+    # Access elements
+    print(my_list[1]) # Prints 7
+    my_list[1] = my_list[0] + my_list[2]
+    print(my_list[1]) # Prints 3
+
+    # my_list[5] = 1 # IndexError
+
+    # Append element
+    my_list.append(21)
+    print(my_list[5])
+    
+    # Insert element
+    my_list.insert(3, -100)
+
+    print(my_list)
+
+    # Delete element
+    del my_list[1]
+
+    print(my_list)
+
+    # Length of list
+    print(len(my_list))
+
+    #for j in range(len(my_list)):
+    #    print(my_list[j] * 2)
+
+    for i in my_list:
+        print(i * 2)
+
+    # Type hints, lists in functions
+    
+    # Modifying list elements in function
+    modify_list(my_list)
+    print(my_list[0]) # Prints 1000!
+
+    # in operator
+
+    # List comprehensions
+
+    # Tracebacks
+
+    # Code style
+
+    # Explicit type annotations of local variables
+
+
+if __name__ == '__main__':
+    main()
